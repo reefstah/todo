f1b08b1a494a696d2628578ecebbd3bccd652dd3
reef <public@ralph.tech>
2022-04-04 22:00:14 +0200

    The beginning of something new

---
+++ src/main.rs
@@ -0,0 +1,106 @@
+use std::env;
+use std::fs;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+use std::process::Command;
+
+use uuid::Uuid;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {
+        content: String,
+    },
+    Edit {
+        todo_id: String,
+        #[clap(short, long)]
+        content: Option<String>,
+    },
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let cli = Cli::parse();
+
+    let todo_dir = Path::new("todo/");
+
+    match cli.command {
+        Some(Commands::Add { content }) => {
+            if !todo_dir.is_dir() {
+                fs::create_dir("todo").unwrap();
+            }
+
+            let uuid = Uuid::new_v4().to_string();
+            let name = todo_dir.to_str().unwrap().to_string() + &uuid;
+
+            let mut file = OpenOptions::new()
+                .read(false)
+                .write(true)
+                .create(true)
+                .open(name)?;
+
+            file.write(content.as_bytes())?;
+
+            println!("Command add invoked {}", content);
+        }
+        Some(Commands::Edit {
+            todo_id,
+            content: None,
+        }) => {
+            let editor = env::var("EDITOR").unwrap();
+            let path = String::from("todo/") + &todo_id;
+            if Path::new(&path).exists() {
+                Command::new(editor).arg(path).spawn().unwrap().wait()?;
+            } else {
+                panic!("File doesn't exist");
+            }
+        }
+        Some(Commands::Edit {
+            todo_id,
+            content: Some(content),
+        }) => {
+            let path = String::from("todo/") + &todo_id;
+            let path = Path::new(&path);
+            let mut file = OpenOptions::new()
+                .read(false)
+                .write(true)
+                .create(false)
+                .open(path)?;
+            println!("{}", &content);
+            file.write(content.as_bytes())?;
+        }
+        None => {
+            for entry in fs::read_dir(todo_dir)? {
+                let entry = entry?;
+                if let Ok(lines) = read_lines(entry.path()) {
+                    for line in lines {
+                        if let Ok(content) = line {
+                            println!("{}, {:?}", content, entry.file_name());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
